7c66dc2d873a340f0644e3a1209dee643ee4ca03
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-16 11:14:04 +0530

    Pyrogram V2.0.xx

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ class channelforward(Client):
     
     def __init__(self):
         super().__init__(
-            session_name="CHANNELFORWARD",
+            name="CHANNELFORWARD",
             bot_token = Config.BOT_TOKEN,
             api_id = Config.API_ID,
             api_hash = Config.API_HASH,
